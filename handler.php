e13bd405f498cd4b2a51bec96eb83d322df19b1c
Serghei Muntean <sergiu1607@gmail.com>
2020-03-14 06:59:35 +0200

    Init project

---
+++ handler.php
@@ -0,0 +1,36 @@
+<?php
+
+// Подключаем файл конфигурации
+include "db.php";
+
+// Получаем значение переменной "search" из файла "script.js".
+if (isset($_POST['search'])) {
+
+    // Помещаем поисковой запрос в переменной
+    $Name = $_POST['search'];
+
+    // Запрос для выбора из базы данных
+    $Query = "SELECT Name FROM users WHERE Name LIKE '%$Name%' LIMIT 5";
+
+    //Производим поиск в базе данных
+    $ExecQuery = mysqli_query($connectionDB, $Query);
+
+    // Создаем список для отображения результатов
+    echo '<ul>';
+
+    //Перебираем результаты из базы данных
+    while ($Result = MySQLi_fetch_array($ExecQuery)) {
+        ?>
+        <!-- Создаем элементы списка. При клике на результат вызываем функцию обработчика fill() из файла "script.js". В параметре передаем найденное имя-->
+
+        <li onclick='fill("<?php echo $Result['Name']; ?>")'>
+            <a>
+                <?php echo $Result['Name']; ?>
+            </a>
+        </li>
+
+        <?php
+    }
+}
+?>
+</ul>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Живой поиск</title>
+    <!-- Подключаем библиотеку jQuery -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <!-- Подключаем наш файл скрптов -->
+    <script type="text/javascript" src="script.js"></script>
+    <!-- Подключаем наш файл стилей-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<form>
+    <!-- Поле поиска -->
+    <label>
+        <input type="text" id="search" placeholder="Поиск людей" />
+    </label>
+</form>
+
+<p>
+    <b>Например: </b>
+    <i>Василий, Татьяна, Наталия, Иван, Андрей</i>
+</p>
+
+<!-- Контейнер для результатов поиска -->
+<div id="display"></div>
+</body>
+</html>
